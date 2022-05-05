f548dcd8cd065453e61bd0ad6ae57c52648675f7
MoOree03 <juanmiguelvasquezmoreno@gmail.com>
2022-05-04 20:19:44 -0500

    FirstCommit

---
+++ src/filesmanagement/FilesManagement.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package filesmanagement;
+
+/**
+ *
+ * @author Julian Lara
+ */
+public class FilesManagement {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        jFrame vnt = new jFrame();
+        vnt.show();
+    }
+    
+}
